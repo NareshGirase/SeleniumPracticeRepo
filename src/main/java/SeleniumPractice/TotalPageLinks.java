ace983f9e97d5826651e5077f957fdd11d52ced3
NareshGirase <naresh.girase21@gmail.com>
2020-06-28 10:23:49 +0530

    First git commit for my selenium code

---
+++ src/main/java/SeleniumPractice/TotalPageLinks.java
@@ -0,0 +1,38 @@
+package SeleniumPractice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Sleeper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TotalPageLinks {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("Https://amazon.in");
+
+		List<WebElement> LinksList = driver.findElements(By.tagName("a"));
+		System.out.println("Total Number of Links:" + LinksList.size());
+
+		for (int i = 0; i < LinksList.size(); i++) {
+			String text = LinksList.get(i).getText();
+			if(text.isEmpty()){
+				System.out.println("No text");
+			}else
+			{
+				System.out.println(text);
+			}
+			
+
+		}
+	}
+
+}
