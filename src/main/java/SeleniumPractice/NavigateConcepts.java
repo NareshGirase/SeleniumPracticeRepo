ee4daf18dcbae3ed57c0995d059e17db7e10bf14
NareshGirase <naresh.girase21@gmail.com>
2020-07-05 16:39:37 +0530

    navigate concept class is updated

--- src/main/java/SeleniumPractice/NavigateConcepts.java
+++ src/main/java/SeleniumPractice/NavigateConcepts.java
@@ -1,9 +1,36 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class NavigateConcepts {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://google.com");
+		
+		System.out.println(driver.getTitle());
+		
+		driver.navigate().to("https://Amazon.in");
+		System.out.println(driver.getTitle());
+		
+		driver.navigate().back();
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(5000);
+		
+		driver.navigate().forward();
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(5000);
+		
+		driver.quit();
+		
 
 	}
 
