cb530323baf3da3f375f47566dab3cd041cb49bc
NareshGirase <naresh.girase21@gmail.com>
2020-07-08 14:36:51 +0530

    8-7-2020

--- src/main/java/SeleniumPractice/AlertPopUpHandle.java
+++ src/main/java/SeleniumPractice/AlertPopUpHandle.java
@@ -9,24 +9,29 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AlertPopUpHandle {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriverManager.chromedriver().setup();
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
-		driver.findElement(By.name("proceed")).click();
-		Alert alert = driver.switchTo().alert();
+		driver.findElement(By.name("proceed")).click();//Click on Go button
+		
+		Thread.sleep(5000);
+		
+		Alert alert = driver.switchTo().alert();//Switch to java script popup
 		String text = alert.getText();
 		System.out.println(text);
 		
-		if(text.equals("Please enter a valid user name"));
+		if(text.equals("Please enter a valid user name"))
 		{
 			System.out.println("Correct error message");
 		}
-		
-			
+		else{
+			System.out.println("Inorrect error message");
+		}
+		alert.accept();//Click on Ok button
 	}
 		
 
