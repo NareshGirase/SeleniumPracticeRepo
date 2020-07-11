70944b78461d3da721b14f868b0622ed236ae107
NareshGirase <naresh.girase21@gmail.com>
2020-07-11 16:17:23 +0530

    updated on 11-07-2020

--- src/main/java/SeleniumPractice/FileUploadPopupConcept.java
+++ src/main/java/SeleniumPractice/FileUploadPopupConcept.java
@@ -1,9 +1,24 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FileUploadPopupConcept {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://cgi-lib.berkeley.edu/ex/fup.html");
+		
+		WebElement Choose = driver.findElement(By.name("upfile"));
+		Choose.sendKeys("D:/Bug Report/1.jpeg"); // code for handle upload file popup but type=file attribute should be there
 
 	}
 
---
+++ src/main/java/SeleniumPractice/WindowsPopupConcept.java
@@ -0,0 +1,93 @@
+package SeleniumPractice;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowsPopupConcept {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.popuptest.com/goodpopups.html");
+		
+		WebElement link= driver.findElement(By.linkText("Good PopUp #3"));
+		link.click();
+		
+		Thread.sleep(3000);
+		Set<String> handles = driver.getWindowHandles();
+		
+		Iterator<String> it = handles.iterator();//Iterator always stored in the string format thats we have stored in string
+		
+		
+		String ParentID = it.next();
+		System.out.println("Parent window ID id :"+ParentID );
+		
+		String ChildID = it.next();
+		System.out.println("Child window ID id :"+ChildID );
+		
+		//String ChildID1 = it.next();
+		//System.out.println("Child window ID id :"+ChildID1 );
+		
+		
+		driver.switchTo().window(ChildID);// driver switch to child window
+		System.out.println(driver.getTitle());
+		
+		driver.close();
+		
+		driver.switchTo().window(ParentID);//driver switch to parent window
+		System.out.println(driver.getTitle());
+		
+		//Thread.sleep(5000);
+		//driver.close();
+		
+		WebElement link1= driver.findElement(By.linkText("Good PopUp #4"));
+		link1.click();
+		
+		Set<String> handles1 = driver.getWindowHandles();
+		
+		Iterator<String> it1 = handles1.iterator();
+		
+		/*if(!ParentID.equals(ChildID))
+		{
+		driver.switchTo().window(ChildID);
+
+		System.out.println(driver.switchTo().window(ChildID).getTitle());
+
+		driver.close();
+		}*/
+
+		
+		String ParentID1 = it1.next();
+		System.out.println("Parent window ID id :"+ParentID1 );
+		
+		String ChildID1 = it1.next();
+		System.out.println("Child window ID id :"+ChildID1 );
+		
+		driver.switchTo().window(ChildID1);// driver switch to child1 window
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(3000);
+		
+		driver.close();
+		
+		driver.switchTo().window(ParentID1);//driver switch to parent window
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(3000);
+		
+		driver.close();
+		
+		
+	}
+
+}
